7a36f93e1f0ac2612518c426669569422fc22788
toundralove <hugo.lemahieu29@gmail.com>
2026-05-07 19:08:18 +0200

    update ecart site

--- zones/zone-02/02_BCK-Path.js
+++ zones/zone-02/02_BCK-Path.js
@@ -2,8 +2,8 @@ window.addEventListener("load", () => {
 
   // 1. Lance Unity
   ECARTLoader.init({
-    buildPath: "../../unity/zone02-V2/Build",
-    buildName: "build-mamco_compress"
+    buildPath: "./02_BACK_01/Build",
+    buildName: "02_BACK_01"
   });
 
   // 2. Active le joystick HTML → Unity
